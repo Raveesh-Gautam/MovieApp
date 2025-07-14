f5c1b5a806491065aa4d213af917128b9e30d635
Raveesh-Gautam <raveeshgautamfzd2002@gmail.com>
2025-07-14 14:47:08 +0530

    cart ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import Cart from './components/Cart/Cart'
+import Header from './components/Header'
 import Products from './components/products/Products'
+
 
 const App = () => {
   return (
     <div>
+  <Header/>
       <Products />
+      <Cart/>
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function Header() {
+    const handleCart=()=>{
+
+    }
+  return (
+    <Navbar expand="lg" className="bg-dark shadow-sm">
+      <Container>
+        <Navbar.Brand href="#home" className="fw-bold text-primary">
+          MyShop
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto ">
+            <Nav.Link href="#home" className="text-light">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#Store" className="text-light">
+              Store
+            </Nav.Link>
+            <Nav.Link href="#About" className="text-light">
+              About
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+
+       <Navbar.Brand className="border border-danger rounded px-3 py-1 text-light fw-semibold" onClick={handleCart}>
+  Cart
+</Navbar.Brand>
+
+        <Navbar.Brand>
+          <span className="badge bg-success">{`0`}</span>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
